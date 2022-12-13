7d72b63313f8c9fe74bdfc635dd6926f7e7e4864
Tomas Rezucha <tomas.rezucha@espressif.com>
2022-12-13 14:11:08 +0100

    Update version to 0.14.1

--- src/tusb_option.h
+++ src/tusb_option.h
@@ -34,7 +34,7 @@ typedef int make_iso_compilers_happy;
 
 #define TUSB_VERSION_MAJOR     0
 #define TUSB_VERSION_MINOR     14
-#define TUSB_VERSION_REVISION  0
+#define TUSB_VERSION_REVISION  1
 #define TUSB_VERSION_STRING    TU_STRING(TUSB_VERSION_MAJOR) "." TU_STRING(TUSB_VERSION_MINOR) "." TU_STRING(TUSB_VERSION_REVISION)
 
 //--------------------------------------------------------------------+
